ea741c0ccb3ea19564f5c5a16ed479842c910cf1
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-03-09 19:44:07 +0100

    Ray caster module

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import config
+import config, raycaster
 import math
 
 TILE_SIZE = 32
@@ -7,10 +7,11 @@ TILE_SIZE = 32
 FOV = 90
 RAYS = config.W//5
 # RAYS = 10
-ALMOST_ZERO = 10**-5
 RAY_GAP = math.radians(FOV/RAYS)
 RAY_DISTANCE = 20
 WALL_HEIGHT = 15
+
+LINE_WIDTH = config.W//RAYS
 
 TILEMAP_SIZE = 256
 
@@ -100,11 +101,10 @@ screen = pg.display.set_mode((config.W, config.H))
 clock = pg.time.Clock()
 quitted = False
 
-brick_texture = pg.image.load("brick.jpg").convert_alpha()
 
 minimap_surf = pg.Surface((config.W, config.H), pg.SRCALPHA)
 
-player = Player((-TILE_SIZE, -TILE_SIZE))
+player = Player((10*TILE_SIZE, 10*TILE_SIZE))
 tiles = [
     [2, 2, 0, 0, 0, 0, 2, 2],
     [2, 0, 0, 0, 0, 0, 0, 2],
@@ -115,13 +115,18 @@ tiles = [
     [2, 0, 0, 0, 0, 0, 0, 3],
     [3, 3, 0, 0, 0, 0, 3, 3],
 ]
+tiles = [[i if i != 0 else None for i in row] for row in tiles]
 
 tilemap_rect = pg.Rect(0, 0, len(tiles)*TILE_SIZE, len(tiles)*TILE_SIZE)
 tilemap_rects = []
 for y, row in enumerate(tiles):
     for x, tile in enumerate(row):
-        if tile != 0:
+        if tile is not None:
             tilemap_rects.append(pg.Rect(x*TILE_SIZE, y*TILE_SIZE, TILE_SIZE, TILE_SIZE))
+
+tilemap = raycaster.TileMap(8, 8, tiles)
+caster = raycaster.Caster(player.get_pos(), player.get_angle(), RAYS, FOV, RAY_DISTANCE)
+
 
 while not quitted:
     dt = clock.tick(config.FPS) / 1000
@@ -139,9 +144,7 @@ while not quitted:
     for rect in tilemap_rects:
         pg.draw.rect(minimap_surf, (255, 255, 255), (rect.x, rect.y, rect.w, rect.h))
 
-    tilemap_size = (len(tiles)*TILE_SIZE, len(tiles)*TILE_SIZE)
     player_pos = player.get_pos()
-    player_angle = player.get_angle()
 
     # The position of the player mapped to the grid.
     # This is stored as a float vector, since it's neccessary to later compute the player's inner-cell position
@@ -151,105 +154,26 @@ while not quitted:
         player_pos.y/TILE_SIZE,
     )
 
-    rect_w = config.W//RAYS
-    for ray in range(RAYS):
-        ray_angle = player_angle-math.radians(FOV/2) + ray * RAY_GAP
-        ray_direction = pg.Vector2(math.cos(ray_angle), math.sin(ray_angle))
+    caster.set_pos(player_grid_pos)
+    caster.set_angle(player.get_angle())
+    raycast_results = raycaster.cast_ray(tilemap, caster)
+
+    for (ray, tile, distance, ray_hit, is_y_side) in raycast_results:
+        tile_ray_distance = distance*TILE_SIZE
+        tile_ray_hit = ray_hit*tile_ray_distance
         
-        # Here we calculate the hypothenuse for each axis.
-        # Basically, to move by unit 1 (our grid cell size), how many "steps" do we need with our
-        # axis direction?
-        # The infinity here is used purely to avoid division by zero
-        if ray_direction.x == 0:
-            ray_direction.x = ALMOST_ZERO
-        if ray_direction.y == 0:
-            ray_direction.y = ALMOST_ZERO
-
-        ray_step = pg.Vector2(
-            math.sqrt(1 + (ray_direction.y / ray_direction.x) ** 2),
-            math.sqrt(1 + (ray_direction.x / ray_direction.y) ** 2)
-        )
-
-        # A grid integer coordinate. We start with the player position
-        # PS: int() for negative values produces a rounding for an opposed direction, which in turn
-        # breaks this algorithm (i.e. 0.75 -> 0, while -0.75 -> 0).
-        # math.floor will consistenly map the floating point to its lowest, even for negative values
-        grid_x, grid_y = math.floor(player_grid_pos.x), math.floor(player_grid_pos.y)
-
-        # A fixed integer grid vector direction, for traversing the grid
-        grid_direction = (
-            1 if ray_direction.x > 0 else -1,
-            1 if ray_direction.y > 0 else -1
-        )
-
-        # Not a vector, but instead a 2 value map that allows us to compare 2 axis.
-        # In DDA, we move and check the smallest axis, then increase its value.
-        # 
-        # Here we need to initialize it to the inner-cell position of the player (between 0 and 1),
-        # to ensure that the ray doesn't start from the grid position, but from the player's.
-        traversed_axis = pg.Vector2(0, 0)
-        if ray_direction.x > 0:
-            traversed_axis.x = (grid_x+1-player_grid_pos.x) * ray_step.x
-        else:
-            traversed_axis.x = (player_grid_pos.x-grid_x) * ray_step.x
-
-        if ray_direction.y > 0:
-            traversed_axis.y = (grid_y+1-player_grid_pos.y) * ray_step.y
-        else:
-            traversed_axis.y = (player_grid_pos.y-grid_y) * ray_step.y
-
-        # A hit stack stores these values: (tile, distance)
-        tile = None
-        ray_distance = 0
-        y_side = False
-        while tile is None and 0 <= ray_distance < RAY_DISTANCE:
-            ray_distance = min(min(traversed_axis.x, traversed_axis.y), RAY_DISTANCE)
-            if traversed_axis.x <= traversed_axis.y:
-                y_side = False
-                traversed_axis.x += ray_step.x
-                grid_x += grid_direction[0]
-            else:
-                y_side = True
-                traversed_axis.y += ray_step.y
-                grid_y += grid_direction[1]
-
-            if 0 <= grid_x < len(tiles) and 0 <= grid_y < len(tiles):
-                if (found_tile := tiles[grid_y][grid_x]) != 0:
-                    tile = found_tile
-        
-        og_distance = ray_distance
-        if tile is not None:
-            # Fish-eye effect
-            ray_distance *= math.cos(player_angle-ray_angle)
-
-            # Honestly, this should panic, but this can happen if a player is stuck inside an object.
-            # In this case, I think I'll keep it at a small amount instead
-            if ray_distance == 0:
-                ray_distance = ALMOST_ZERO
-
-            tile_ray_distance = ray_distance*TILE_SIZE
-            ray_hit = player_pos+ray_direction*tile_ray_distance
-            
-            dist = config.H/tile_ray_distance
-            color = COLOR_MAP[tile]
-            # color = tuple([int(channel*(1-ray_distance/RAY_DISTANCE)) for channel in color])
-            if y_side:
-                color = tuple([int(channel//2) for channel in color])
-            rect_h = int(dist*TILE_SIZE)
-            # Rendering the rectangle
-            # screen.blit(brick_texture.subsurface())
-            pg.draw.line(screen, (255, 0, 0), player_pos, ray_hit, 1)
-            pg.draw.circle(screen, (0, 255, 0), ray_hit, 2)
-            pg.draw.rect(screen, color, (ray*rect_w, config.H//2-rect_h//2, rect_w, rect_h))
-            # tw, th = brick_texture.get_size()
-            # cell_dist = math.ceil(og_distance)-og_distance
-            # sub_texture = brick_texture.subsurface(cell_dist * tw, 0, min(int(cell_dist*tw)-1, int(cell_dist*tw)+5), th)
-            # screen.blit(brick_texture, (ray*rect_w, config.H//2-rect_h//2))
-        else:
-            pg.draw.line(screen, (255, 0, 0), player_pos, player_pos + ray_direction*RAY_DISTANCE*TILE_SIZE, 1)
+        dist = config.H/tile_ray_distance
+        color = COLOR_MAP[tile]
+        color = tuple([int(channel*(1-distance/RAY_DISTANCE)) for channel in color])
+        if is_y_side:
+            color = tuple([int(channel//2) for channel in color])
+        rect_h = int(dist*TILE_SIZE)
+        # Rendering the rectangle
+        pg.draw.rect(screen, color, (ray*LINE_WIDTH, config.H//2-rect_h//2, LINE_WIDTH, rect_h))
 
     player.draw(minimap_surf)
     screen.blit(minimap_surf, (0, 0))
+
     pg.display.flip()
 
 pg.quit()
---
+++ raycaster.py
@@ -0,0 +1,140 @@
+import pygame as pg
+import math
+
+ALMOST_ZERO = 10**-5
+
+class TileMap:
+    def __init__(self, width: int, height: int, tiles: list[list[int | None]] = None):
+        self.width = width
+        self.height = height
+        # Generate an empty tileset
+        if tiles is not None:
+            assert len(tiles) == self.width
+            assert len(tiles[0]) == self.height
+            self.tiles = tiles 
+        else:
+            self.tiles = [[None for _ in range(width)] for _ in range(height)]
+
+    def get_size(self) -> tuple[int, int]:
+        return (self.width, self.height)
+    
+    def get_tiles(self) -> list[list[None | int]]:
+        return self.tiles
+
+class Caster:
+    """
+    A caster is an object that allows storing data, important for casting rays.
+
+    """
+    def __init__(
+            self, 
+            pos: pg.Vector2, 
+            angle: float, 
+            rays: int, 
+            fov: int, 
+            max_ray_distance: int
+            ):
+        self.pos = pos.copy() # The position is in tile coordinates 
+        self.angle = angle
+        self.fov = fov
+
+        assert rays > 0
+        self.rays = rays
+
+        assert fov > 0
+        self.fov = fov
+
+        self.ray_gap = math.radians(self.fov/self.rays)
+
+        assert max_ray_distance > 0
+        self.max_ray_distance = max_ray_distance
+
+    def set_pos(self, new_pos: pg.Vector2):
+        self.pos = new_pos
+
+    def set_angle(self, angle: float):
+        self.angle = angle
+
+def cast_ray(tilemap: TileMap, caster: Caster) -> list[(int, int, float, pg.Vector2, False)]:
+    "Cast a ray and return a list of results. A result is a tuple of: `(ray_ind, data (ID of the tile), distance, ray_hit_pos, is_y_side)`"
+    caster_pos = caster.pos
+    caster_angle = caster.angle
+
+    tiles = tilemap.get_tiles()
+    map_w, map_h = tilemap.get_size()
+
+    results = []
+    for ray in range(caster.rays):
+        ray_angle = caster_angle-math.radians(caster.fov/2) + ray * caster.ray_gap
+        ray_direction = pg.Vector2(math.cos(ray_angle), math.sin(ray_angle))
+        
+        # Here we calculate the hypothenuse for each axis.
+        # Basically, to move by unit 1 (our grid cell size), how many "steps" do we need with our
+        # axis direction?
+        # The infinity here is used purely to avoid division by zero
+        if ray_direction.x == 0:
+            ray_direction.x = ALMOST_ZERO
+        if ray_direction.y == 0:
+            ray_direction.y = ALMOST_ZERO
+
+        ray_step = pg.Vector2(
+            math.sqrt(1 + (ray_direction.y / ray_direction.x) ** 2),
+            math.sqrt(1 + (ray_direction.x / ray_direction.y) ** 2)
+        )
+
+        # A grid integer coordinate. We start with the player position
+        # PS: int() for negative values produces a rounding for an opposed direction, which in turn
+        # breaks this algorithm (i.e. 0.75 -> 0, while -0.75 -> 0).
+        # math.floor will consistenly map the floating point to its lowest, even for negative values
+        grid_x, grid_y = math.floor(caster_pos.x), math.floor(caster_pos.y)
+
+        # A fixed integer grid vector direction, for traversing the grid
+        grid_direction = (
+            1 if ray_direction.x > 0 else -1,
+            1 if ray_direction.y > 0 else -1
+        )
+
+        # Not a vector, but instead a 2 value map that allows us to compare 2 axis.
+        # In DDA, we move and check the smallest axis, then increase its value.
+        # 
+        # Here we need to initialize it to the inner-cell position of the player (between 0 and 1),
+        # to ensure that the ray doesn't start from the grid position, but from the player's.
+        traversed_axis = pg.Vector2(0, 0)
+        if ray_direction.x > 0:
+            traversed_axis.x = (grid_x+1-caster_pos.x) * ray_step.x
+        else:
+            traversed_axis.x = (caster_pos.x-grid_x) * ray_step.x
+
+        if ray_direction.y > 0:
+            traversed_axis.y = (grid_y+1-caster_pos.y) * ray_step.y
+        else:
+            traversed_axis.y = (caster_pos.y-grid_y) * ray_step.y
+
+        tile = None
+        ray_distance = 0
+        y_side = False
+        while tile is None and 0 <= ray_distance < caster.max_ray_distance:
+            ray_distance = min(min(traversed_axis.x, traversed_axis.y), caster.max_ray_distance)
+            if traversed_axis.x <= traversed_axis.y:
+                y_side = False
+                traversed_axis.x += ray_step.x
+                grid_x += grid_direction[0]
+            else:
+                y_side = True
+                traversed_axis.y += ray_step.y
+                grid_y += grid_direction[1]
+
+            if 0 <= grid_x < map_w and 0 <= grid_y < map_h:
+                if (found_tile := tiles[grid_y][grid_x]) != None:
+                    tile = found_tile
+        
+        if tile is not None:
+            if ray_distance == 0:
+                ray_distance = ALMOST_ZERO
+
+            ray_hit = caster_pos+ray_direction*ray_distance
+            ray_distance *= math.cos(caster_angle-ray_angle)
+
+            results.append((ray, tile, ray_distance, ray_hit, y_side))
+
+    return results
