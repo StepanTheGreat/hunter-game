0f538896e852eeba71c0b81fe5f06502d413db12
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-03-30 15:12:28 +0200

    Use screen resolution instead for the menu

--- src/plugins/scenes/mainmenu/__init__.py
+++ src/plugins/scenes/mainmenu/__init__.py
@@ -3,6 +3,7 @@ import pygame as pg
 from plugin import Resources, Plugin, Schedule, run_if, resource_exists
 
 from core.assets import AssetManager
+from core.pg import Screen
 
 from modules.scene import SceneManager, SceneBundle
 
@@ -18,11 +19,13 @@ class MainMenu:
 def draw_main_menu(resources: Resources):
     menu = resources[MainMenu]
     renderer = resources[Renderer2D]
-
+    screen = resources[Screen]
+    
+    w, h = screen.get_size()
     textw, texth = menu.font.measure("You're in main menu!")
 
-    renderer.draw_rect((0, 0, 700, 700), (0.2, 0.2, 0.2))
-    renderer.draw_text(menu.font, "You're in main menu!", (350-textw/2, 350-texth/2), (1, 1, 1), 1)
+    renderer.draw_rect((0, 0, w, h), (0.2, 0.2, 0.2))
+    renderer.draw_text(menu.font, "You're in main menu!", (w/2-textw/2, h/2-texth/2), (1, 1, 1), 1)
 
 class MainMenuScene(SceneBundle):
     def __init__(self, resources: Resources):
