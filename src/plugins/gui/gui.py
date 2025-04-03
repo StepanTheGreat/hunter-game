72ed749ee7011ae4fb4bc8889c3b130ca9a2a49c
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-04-03 23:49:30 +0200

    Explain GUI box stacking

--- src/plugins/gui/gui.py
+++ src/plugins/gui/gui.py
@@ -8,14 +8,50 @@ top (or below) each other.
 [button2]
 ```
 
-In this case we have a `button1` box, to which we have attached `button2` box below and `input` box to the right.
+In this idea we have a `button1` box, to which we have attached `button2` box below and `input` box to the right.
 This is essentially what most GUI libraries do, but here we avoid the concept of a layour container entirely.
 
-This concept allows us to easily model custom GUI elements, recalculate layout, customize layout
-direction and so much more
-"""
+This idea allows us to easily model custom GUI elements, recalculate layout, customize layout
+direction and so much more.
 
-# TODO: Add more documentation on how this GUI system works
+There are 2 concepts that allow us to "stack" our elements onto another: its edge and pivot coordinates.
+
+## Edges
+Edge coordinates while difficult to explain in simple terms, are coordinates that define our inner rectangle area
+in range between `0` and `1`.
+
+The top-left corner of a rectangle has an adge `0, 0`, top-right: `1, 0`, bottom-left: `0, 1` and so on.
+With these 2 coordinates we can define at which point on the rectangle we would like to place our element. Usually
+it should be an edge, but you can also place it inside the parent (though I don't see any reason to do that).
+
+## Anchoring
+Anchor or pivot coordinates, are coordinates that define our rectangle's center. If we represent our rectangle size in range
+from `0,0` to `1, 1`, our center will be `0.5, 0.5`. This is a highly simple concept, but in combination
+with edges we can put our element at ANY position we want.
+
+## Parents/Children
+Box stacking doesn't really make sense if we can't stack multiple boxes on top of each other, right?
+
+A root element (without a parent) is positioned at absolute coordinates (in pixels), though their pivot is still
+used when centering their rectangle.
+
+Child elements however, are placed based on the attanched parent's box using both their edge and pivot coordinates.
+
+An element can have an unlimited amount of children, but only one parent (or none).
+
+## Nuances
+Box stacking, compared to common GUI layout designs like containers has 1 distinct difference:
+the size of a child's container doesn't affect the parent's.
+In most layout designs a child will always affect the parent's boundary box, but since in our design things like
+containers simply don't exist - this is impossible. 
+
+And since containers are non-existent, well... we can't do container things, like say wrapping a background
+texture over container items.
+
+There are solutions to this, as we can add simple bubbling-up message passing whenever a child has recalculated
+its rectangle and then compute our container's bounding box based on the most distant child coordinates, but, 
+even this has some performance cost.
+"""
 
 import pygame as pg
 
