d44cb9f5547e73e51d25bf94d9e4b065c2e92a6e
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-04-12 19:22:26 +0200

    Door?

--- src/plugins/entities/door.py
+++ src/plugins/entities/door.py
@@ -1,13 +1,11 @@
 import pygame as pg
 import moderngl as gl 
 
-from .player import Player
-
 from core.entity import Entity
 from core.collisions import StaticCollider, CollisionManager
 from core.assets import AssetManager
 
-from plugins.graphics import SpriteRenderer, Sprite
+from core.graphics import *
 
 class Door(Entity):
     HITBOX_SIZE = 24
@@ -17,24 +15,13 @@ class Door(Entity):
             pos: tuple[float, float], 
             assets: AssetManager, 
             collisions: CollisionManager,
-            sprites: SpriteRenderer
     ):
-        self.texture = assets.load(gl.Texture, "images/character.png")
-        self.sprite = Sprite(self.texture, pg.Vector2(pos), (48, 48), (0, 0, 1, 1))
-        sprites.push_sprite(self.sprite)
+        self.texture = assets.load(gl.Texture, "images/door.png")
 
-        self.collider = StaticCollider(*pos, Door.HITBOX_SIZE, Door.HITBOX_SIZE)
+        self.collider = StaticCollider(*pos, 48, 12)
         self.player_list = []
 
         collisions.add_collider(self.collider)
-
-    def bind_player_list(self, player_list: list[Player]):
-        """
-        Bind this player list to the sprite so it can target players. 
-        This is an essential operation, as without this method, the sprite wouldn't be able to find and 
-        follow the player.
-        """
-        self.player_list = player_list
 
     def update(self, dt, alpha):
         pass
@@ -44,19 +31,3 @@ class Door(Entity):
 
     def get_pos(self) -> pg.Vector2:
         return pg.Vector2(self.collider.rect.center)
-
-    def draw(self, renderer: SpriteRenderer):
-        renderer.push_sprite(
-            self.texture,
-            self.get_pos(),
-            pg.Vector2(48, 48),
-            (0, 0, 1, 1)
-        )
-    
-# def spawn_sprite(resources: Resources):
-#     entities = resources[EntityWorld]
-
-#     for i in range(5):
-#         entities.push_entity(
-#             Sprite(entities.get_entity_uid(), (200*i, 0), resources)
-#         )
