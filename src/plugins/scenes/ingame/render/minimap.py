e03183c2b7ffc90915e08e1a07b8790e6556dce6
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-04-28 15:39:08 +0200

    Idk why, but cull minimap geometry that's outside the screen (minimap is a dev-only feature)

--- src/plugins/scenes/ingame/render/minimap.py
+++ src/plugins/scenes/ingame/render/minimap.py
@@ -1,6 +1,7 @@
 from plugin import Resources, Schedule, Plugin
 
 from core.ecs import WorldECS
+from core.pg import Screen
 
 from plugins.components import Position, RenderPosition
 from plugins.collisions import StaticCollider, DynCollider
@@ -13,25 +14,34 @@ MINIMAP_SCALE = 0.5
 
 def draw_minimap(resources: Resources):
     renderer = resources[Renderer2D]
+    screen = resources[Screen]
     world = resources[WorldECS]
+
+    width, height = screen.get_size()
 
     scale = MINIMAP_SCALE
 
     rects, circles = [], []
     for ent, (pos, collider) in world.query_components(Position, StaticCollider):
-        pos = pos.get_position()*scale
+        x, y = pos.get_position()*scale
+        w, h = collider.rect.w*scale, collider.rect.h*scale
 
-        rects.append((
-            (pos.x, pos.y, collider.rect.w*scale, collider.rect.h*scale), 
-            (40, 40, 40)
-        ))
+        if (x+w >= 0 and x < width) and (y+h >= 0 and y < height):
+            rects.append((
+                (x, y, w, h), 
+                (40, 40, 40)
+            ))
 
     for ent, (pos, collider) in world.query_components(RenderPosition, DynCollider):
-        pos = pos.get_position()*scale
-        color = (0, 255, 0) if world.has_component(ent, Player) else (255, 0, 0)
-        circles.append((
-            (pos.x, pos.y), collider.radius*MINIMAP_SCALE, color
-        ))
+        x, y = pos.get_position()*scale
+        r = collider.radius*MINIMAP_SCALE
+
+        if (x+r >= 0 and x < width) and (y+r >= 0 and y < height):
+            color = (0, 255, 0) if world.has_component(ent, Player) else (255, 0, 0)
+
+            circles.append((
+                (x, y), r, color
+            ))
     
     if rects:
         renderer.draw_rects(rects)
