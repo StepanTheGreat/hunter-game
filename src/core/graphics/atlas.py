2da4f2060269a7e547f50e59d174f70275d4332a
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-04-29 23:10:10 +0200

    Better atlas loading and handling

--- src/core/graphics/atlas.py
+++ src/core/graphics/atlas.py
@@ -1,6 +1,8 @@
 import moderngl as gl
 import pygame as pg
 
+from json import loads
+
 from plugin import Plugin, Resources
 from math import log2, ceil
 from file import load_file_str
@@ -8,7 +10,6 @@ from file import load_file_str
 from core.graphics.ctx import *
 from core.assets import AssetManager, add_loaders
 
-from modules.config import typed_dataclass, load_config, TypedDataclassTypeMismatch
 from modules.atlas import SurfaceAtlas, SpriteRect
 
 from typing import Optional, Any, Union
@@ -118,15 +119,6 @@ class TextureAtlas:
         if self._cached_texture is not None:
             self._cached_texture.release()
 
-@typed_dataclass
-class StaticTextureAtlas:
-    source: str
-    sprites: dict
-
-@typed_dataclass
-class DynamicTextureAtlas:
-    sources: dict
-
 def _closest_pow2_size(image_size: tuple[int, int]) -> tuple[int, int]:
     """
     For any image size, transform it to the closest possible power-of-2 image size, whose axis
@@ -143,22 +135,36 @@ def _closest_pow2_size(image_size: tuple[int, int]) -> tuple[int, int]:
     w, h = image_size
     return 2 ** ceil(log2(w)), 2 ** ceil(log2(h))
 
+def _assert_key(d: dict, key: str, ty: type) -> Any:
+    "Assert a key in a dictionary and return it"
+
+    assert key in d, f"The key `{key}` isn't present in the object"
+
+    value = d[key]
+    
+    assert type(value) is ty, f"The type of `{key}` should be `{ty}`"
+
+    return value
+
 def _load_static_texture_atlas(
     ctx: gl.Context, 
     assets: AssetManager, 
-    atlas: StaticTextureAtlas
+    atlas: dict
 ) -> TextureAtlas:
     # What we're going to do is SUPER stupid, but I really don't want to optimize this.
     # I think we will just rebuild the entire atlas. Also, we will increase a bit the texture size, since
     # we need power-of-2 textures
 
-    img = pg.image.load(assets.asset_path(atlas.source))
+    atlas_source: str = _assert_key(atlas, "source", str)
+    sprites: dict[str, tuple[int, int, int, int]] = _assert_key(atlas, "sprites", dict)
+    img = pg.image.load(assets.asset_path(atlas_source))
 
     texture_size = _closest_pow2_size(img.get_size())
     new_atlas = TextureAtlas(ctx, texture_size, False, max(texture_size))
 
-    for sprite_key, sprite_region in atlas.sprites.items():
+    for sprite_key, sprite_region in sprites.items():
         assert type(sprite_region) is tuple and len(sprite_region) == 4, "Invalid sprite format used in a static texture atlas"
+
         (x, y, w, h) = sprite_region
 
         new_atlas.push_sprite(sprite_key, img.subsurface(x, y, w, h))
@@ -168,15 +174,13 @@ def _load_static_texture_atlas(
 def _load_dynamic_texture_atlas(
     ctx: gl.Context, 
     assets: AssetManager, 
-    atlas: DynamicTextureAtlas
+    atlas: dict
 ) -> TextureAtlas:
-    
-    # Now, we're going to create the smallest possible base texture, and try fit
-    # all sprites onto it
+    sources: dict[str, str] = _assert_key(atlas, "sources", dict)
 
     new_atlas = TextureAtlas(ctx, DYNAMIC_ATLAS_MIN_SIZE, True, DYNAMIC_ATLAS_MAX_SIZE)
 
-    for source_key, source_path in atlas.sources.items():
+    for source_key, source_path in sources.items():
         assert type(source_path) is str, "Invalid sprite format used in a dynamic texture atlas"
 
         img = pg.image.load(assets.asset_path(source_path))
@@ -196,12 +200,16 @@ def _load_texture_atlas(
 
     If the format doesn't match anything - will raise an error.
     """
-    try:
-        atlas = load_config(StaticTextureAtlas, atlas_data)
-        return _load_static_texture_atlas(ctx, assets, atlas)
-    except (TypeError, TypedDataclassTypeMismatch, AssertionError):
-        atlas = load_config(DynamicTextureAtlas, atlas_data)
-        return _load_dynamic_texture_atlas(ctx, assets, atlas)
+    atlas_obj = loads(atlas_data)
+    assert "dynamic" in atlas_obj, "The atlas should tell if it's dynamic or not via `dynamic: true/false`"
+
+    dynamic = atlas_obj["dynamic"]
+    assert type(dynamic) is bool
+
+    if dynamic:
+        return _load_dynamic_texture_atlas(ctx, assets, atlas_obj)
+    else:
+        return _load_static_texture_atlas(ctx, assets, atlas_obj)
 
 def loader_texture_atlas(resources: Resources, path: str) -> TextureAtlas:
     "A loader for texture atlases"
