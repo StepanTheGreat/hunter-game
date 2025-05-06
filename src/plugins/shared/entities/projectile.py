c3bcd85b6b5dc39cfd8625f094a78ef43794f962
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-05-06 21:24:47 +0200

    Unify projectile hit logic across both actors. On the server, listen for `ProjectileHit` and then apply damage

--- src/plugins/shared/entities/projectile.py
+++ src/plugins/shared/entities/projectile.py
@@ -99,6 +99,45 @@ def make_projectile(
 ) -> tuple:
     return factory.make_projectile(pos, direction)
 
+@event
+class ProjectileHitEvent:
+    "Fired whenever a projectile hits a hittable entity of an opposite team"
+
+    def __init__(self, target_ent: int, damage: float):
+        self.target_ent = target_ent
+        self.damage = damage
+
+def collide_projectiles(resources: Resources, event: CollisionEvent):
+    world = resources[WorldECS]
+    ewriter = resources[EventWriter]
+
+    projectile_entity, target_entity = event.sensor_entity, event.hit_entity
+
+    if not world.contains_entities(target_entity, projectile_entity):
+        return
+
+    # First we make sure that the sensor entity is a projectile
+
+    if world.has_component(projectile_entity, Projectile):
+        if event.hit_collider_ty is StaticCollider:
+            # The projectile has hit a wall - kill him!
+            world.remove_entity(projectile_entity)
+        elif world.has_components(target_entity, Hittable, Team, Health):
+            projectile_team, projectile = world.get_components(projectile_entity, Team, Projectile)
+            target_team = world.get_component(target_entity, Team)
+
+            # If they are on the same team however - we won't do anything
+            if not projectile_team.same_team(target_team):
+
+                # Because this entire collision resolution logic is shared, but only the hitting
+                # part is up to the server - we're going to simply send an event instead
+                ewriter.push_event(ProjectileHitEvent(target_entity, projectile.damage))
+
+                # Our projectile can hit multiple targets, which is called piercing!
+                projectile.consume_pierce()
+                if not projectile.can_pierce():
+                    world.remove_entity(projectile_entity)
+
 class ProjectilePlugin(Plugin):
     def build(self, app):
-        pass
+        app.add_event_listener(CollisionEvent, collide_projectiles)
