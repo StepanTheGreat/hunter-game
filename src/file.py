d8c198bf8dbffaceae7a2063c147f0421c1ee507
StepanTheGreat <117771383+StepanTheGreat@users.noreply.github.com>
2025-05-09 23:10:17 +0200

    Explain PROJECT_DIR thing

--- src/file.py
+++ src/file.py
@@ -7,11 +7,21 @@ if getattr(sys, "frozen", False):
     PROJECT_DIR = sys._MEIPASS + "/"
 else:
     PROJECT_DIR = abspath(__file__ + "/../../") + "/"
-"""
-This is an ugly way of solving it, but since `file.py` is in the same path as `main.py`,
-we can take this absolute path, go back to `src` using `..`, then finally move to any other local 
-file we want without worrying about the directory from which the user has run our app.
-"""
+
+# To explain this monstrosity... We would like to support 2 ways of executing our game - via python,
+# and via pyinstaller binaries. In case of python, our execution directory can be different from the
+# one where `main.py` is located, thus relying on relative paths like for assets or configs is just
+# impossible. Using this strange `abspath(__file__ + "/../../")` we're essentially saying that taking
+# the file path of this `file.py` location, we would like to go 2 directories back, and set it as our
+# project directory.
+#
+# For the pyinstaller thing, pyinstaller in the bundled python runtime sets a special attribute in the
+# `sys` package called `frozen`, which means that the app is running as an executable. Pyinstaller
+# allows us to bundle everything (even our assets) into a SINGLE executable file, which is EXTREMELY
+# convenient for project sharing. It does so by unbundling everything at runtime in a temporary
+# OS folder before executing our app. `_MEIPASS` is that exact directory, which we will use if
+# we're running a frozen application
+
 
 def localize_path(path: str):
     "Join an arbitrary path from the project directory"
